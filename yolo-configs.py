dfa1d236623c7cd081bf1065e6bd5c35c3f04a36
elif-celik <mervelifcelik@gmail.com>
2021-01-01 22:36:16 +0300

    updated

--- yolo-configs.py
+++ yolo-configs.py
@@ -1,6 +1,8 @@
-from imagepreprocessing.darknet_functions import create_training_data_yolo
-
+from imagepreprocessing.darknet_functions import create_training_data_yolo, create_cfg_file_yolo
+'''
 source_path = "/Users/elifcelik/Desktop/train"
 create_training_data_yolo(source_path, percent_to_use=1, validation_split=0.2, 
 create_cfg_file=True, train_machine_path_sep="/", 
-shuffle=True, files_to_exclude=[".DS_Store","train.txt","test.txt","obj.names","obj.data","yolo-obj.cfg"])
+shuffle=True, files_to_exclude=[".DS_Store","train.txt","test.txt","obj.names","obj.data","yolo-obj.cfg"])'''
+
+create_cfg_file_yolo("",12,yolo_version=4)
